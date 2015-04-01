b18536526eed3411238501b2b110a78611c38e41
TheFakeMontyOnTheRun <danielmonteiro@id.uff.br>
2015-03-31 21:29:29 -0300

    finally enables the local partitioning!

--- level-editor-core-java/src/main/java/br/odb/derelict/GenericTreeGeometryCompiler.java
+++ level-editor-core-java/src/main/java/br/odb/derelict/GenericTreeGeometryCompiler.java
@@ -8,17 +8,17 @@ import br.odb.worldprocessing.RemoveCoincidantSectors;
 import br.odb.worldprocessing.RemoveLeafSectors;
 import br.odb.worldprocessing.SectorLinker;
 import br.odb.worldprocessing.WorldGlobalPartitioner;
+import br.odb.worldprocessing.WorldLocalPartitioner;
 
 public class GenericTreeGeometryCompiler extends GeometryCompiler {
 
 	public GenericTreeGeometryCompiler(ApplicationClient client,
 			World worldToProcess) {
 		super(client, worldToProcess);
-		
 		this.processingPipeline.add( new GroupSectorSnapper(client, worldToProcess) );
 		this.processingPipeline.add( new RemoveLeafSectors(client, worldToProcess) );
 		this.processingPipeline.add( new DegenerateSectorCuller(client, worldToProcess) );
-		this.processingPipeline.add( new WorldGlobalPartitioner(client, worldToProcess) );
+		this.processingPipeline.add( new WorldLocalPartitioner(client, worldToProcess) );
 		this.processingPipeline.add( new RemoveCoincidantSectors(client, worldToProcess) );
 		this.processingPipeline.add( new DegenerateSectorCuller(client, worldToProcess));
 		this.processingPipeline.add( new SectorLinker(client, worldToProcess));
